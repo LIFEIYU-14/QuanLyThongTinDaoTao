40f4f90c5942ec65838dc21f08483d2eaae1b94c
mONESY14 <tn216003@gmail.com>
2025-03-13 15:36:47 +0700

    13/03

---
+++ QuanLyThongTinDaoTao/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,25 @@
+﻿using QuanLyThongTinDaoTao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace QuanLyThongTinDaoTao.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        DbContextThongTinDaoTao db = new DbContextThongTinDaoTao(); 
+        // GET: Admin/HomeAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult KhoaHoc()
+        {
+            var khoaHoc = db.KhoaHocs.ToList();
+            return View(khoaHoc);
+        }
+    }
+}
